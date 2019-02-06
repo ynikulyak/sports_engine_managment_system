763c5f172c1034d45b683501c20d6fd7a14f9b1b
Lacey Sikes <lsikes@calpoly.edu>
2019-02-05 16:02:42 -0800

    added views to sql_queries

--- sql/sql_queries.sql
+++ sql/sql_queries.sql
@@ -4,12 +4,39 @@ SELECT * FROM division;
 SELECT * FROM player;
 SELECT * FROM sports;
 SELECT * FROM team;
+-- View 1
+-- Number of players for each team
+CREATE VIEW TEAM_PLAYERS_COUNT AS
+SELECT t.team_id, t.team_name, COUNT(p.player_id) AS number_of_players
+FROM player p, team t
+WHERE p.team_id = t.team_id
+GROUP BY t.team_id, t.team_name;
+
+-- View 2
+-- Number of players for each sport
+CREATE VIEW SPORT_PLAYERS_COUNT AS
+SELECT s.sport_id, s.sport_name, COUNT(p.player_id) AS number_of_players
+FROM player p, sports s
+WHERE p.sport_id = s.sport_id
+GROUP BY s.sport_id, s.sport_name;
+
+-- View 3
+-- Number of players for each division
+CREATE VIEW DIVISION_PLAYERS_COUNT AS
+SELECT d.division_id, d.division_name, COUNT(p.player_id) AS number_of_players
+FROM player p, division d
+WHERE p.division_id = d.division_id
+GROUP BY d.division_id, d.division_name;
+
 -- 1
 -- To find how many players are on each team, execute:
 SELECT t.team_id, t.team_name, COUNT(p.player_id) AS number_of_players
 FROM player p, team t
 WHERE p.team_id = t.team_id
 GROUP BY t.team_id, t.team_name;
+
+-- OR simply use View:
+SELECT * FROM TEAM_PLAYERS_COUNT;
 
 -- 2
 -- To see how many players there are per division, execute:
@@ -18,12 +45,18 @@ FROM player p, division d
 WHERE p.division_id = d.division_id
 GROUP BY d.division_id, d.division_name;
 
+-- OR simply use View:
+SELECT * FROM DIVISION_PLAYERS_COUNT;
+
 -- 3
 -- To see how many players there are per sport,execute:
 SELECT s.sport_id, s.sport_name, COUNT(p.player_id) AS number_of_players
 FROM player p, sports s
 WHERE p.sport_id = s.sport_id
 GROUP BY s.sport_id, s.sport_name;
+
+-- OR simply use View:
+SELECT * FROM SPORT_PLAYERS_COUNT;
 
 -- 4
 -- To count how many Right fielders each sport has, execute:
